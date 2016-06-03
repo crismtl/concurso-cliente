662c6c1ac2c161c4faf27da034c61d9b4f0b9d04
Concurso <Concurso>
2016-06-03 14:30:50 -0500

    Final 2 modulos

--- js/app.js
+++ js/app.js
@@ -1,22 +1,23 @@
-var app = angular.module('mipanel', ['ui.router']);
-app.config(function($stateProvider, $urlRouterProvider) {
-    // angular.extend(toastrConfig, {
-    //     autoDismiss: false,
-    //     containerId: 'toast-container',
-    //     maxOpened: 0,
-    //     newestOnTop: true,
-    //     positionClass: 'toast-bottom-right',
-    //     preventDuplicates: false,
-    //     preventOpenDuplicates: false,
-    //     target: 'body'
-    // });
+var app = angular.module('mipanel', ['ui.router', 'ngResource', 'toastr', 'ngCookies']);
+app.config(function($stateProvider, $urlRouterProvider, toastrConfig) {
+    angular.extend(toastrConfig, {
+        autoDismiss: false,
+        containerId: 'toast-container',
+        maxOpened: 0,
+        newestOnTop: true,
+        positionClass: 'toast-bottom-right',
+        preventDuplicates: false,
+        preventOpenDuplicates: false,
+        target: 'body'
+    });
+
     $stateProvider
         .state('home', {
             url: "/",
             templateUrl: "vistas/inicio.html",
-            // data: {
-            //     loginRequerido: false
-            // }
+            data: {
+                loginRequerido: false
+            }
         })
         // .state('acercade', {
         //     url: "/acercade",
@@ -29,58 +30,41 @@ app.config(function($stateProvider, $urlRouterProvider) {
             url: "/solicitud",
             templateUrl: "vistas/solicitud.html",
             controller: 'SolicitudCtrl',
-            // data: {
-            //     loginRequerido: false
-            // }
+            data: {
+                loginRequerido: true
+            }
         })
-        // .state('login', {
-        //     url: "/login",
-        //     templateUrl: "rutas/login.html",
-        //     controller: 'LoginController',
-        //     // data: {
-        //     //     loginRequerido: false
-        //     // }
-        // })
-        // .state('miperfil', {
-        //     url: "/miperfil",
-        //     templateUrl: "rutas/miperfil.html",
-        //     controller: 'MiPerfilController',
-        //     // data: {
-        //     //     loginRequerido: true
-        //     // }
-        // })
-        // .state('canvas', {
-        //     url: "/canvas",
-        //     templateUrl: "rutas/canvas.html",
-        //     controller: 'CanvasController',
-        //     // data: {
-        //     //     loginRequerido: true
-        //     // }
-        // })
-        // .state('chat', {
-        //     url: "/chat",
-        //     templateUrl: "rutas/chat.html",
-        //     controller: 'ChatController',
-        //     // data: {
-        //     //     loginRequerido: true
-        //     // }
-        // });
-        ;
+        .state('consulta', {
+            url: "/consulta",
+            templateUrl: "vistas/consulta.html",
+            controller: 'ConsultaCtrl',
+            data: {
+                loginRequerido: true
+            }
+        })
+        .state('login', {
+            url: "/login",
+            templateUrl: "vistas/ingreso.html",
+            controller: 'IngresoCtrl',
+            data: {
+                loginRequerido: false
+            }
+        });
     $urlRouterProvider.otherwise("/");
 });
 
-// app.run(function($rootScope, $cookies, $state, toastr) {
-//     $rootScope.$on('$stateChangeStart', function(event, toState, toParams) {
-//         var requiereLogin = toState.data.loginRequerido;
-//
-//         if (requiereLogin) {
-//             if ($cookies.get('UsuarioId')) {
-//                 toastr.success('Felicidades se encuentra logueado', 'Éxito');
-//             } else {
-//                 toastr.info('Necesita estar logueado para poder acceder a esta vista', 'Información');
-//                 event.preventDefault();
-//                 return $state.go('login')
-//             }
-//         }
-//     });
-// });
+app.run(function($rootScope, $cookies, $state, toastr) {
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams) {
+        var requiereLogin = toState.data.loginRequerido;
+
+        if (requiereLogin) {
+            if ($cookies.get('UsuarioId')) {
+                toastr.success('Felicidades se encuentra logueado', 'Éxito');
+            } else {
+                toastr.info('Necesita estar logueado para poder acceder a esta vista', 'Información');
+                event.preventDefault();
+                return $state.go('login')
+            }
+        }
+    });
+});
--- js/controladores/SolicitudController.js
+++ js/controladores/SolicitudController.js
@@ -1,45 +1,94 @@
-app.controller('SolicitudCtrl', ['$scope', '$state', function ($scope, $state) {
+app.controller('SolicitudCtrl', ['$scope', '$state', 'SolicitudFactory', 'toastr', function($scope, $state, SolicitudFactory, toastr) {
 
     $scope.nuevaSolicitud = {
-      sol_ci: '',
-      sol_apellidos: '',
-      sol_nombres: '',
-      sol_codprov: '',
-      sol_codciu: '',
-      sol_sexo: '',
-      sol_fechanac: '',
-      sol_nummiem: '',
-      sol_disc: '',
-      sol_telefono: '',
-      sol_fecha: '',
-      sol_estado: '',
-      sol_fecha: '',
-      sol_estado: ''
+        sol_ci: '',
+        sol_apellidos: '',
+        sol_nombres: '',
+        sol_codprov: '',
+        sol_codciu: '',
+        sol_sexo: '',
+        sol_fechanac: '',
+        sol_nummiem: '',
+        sol_disc: '',
+        sol_telefono: '',
+        sol_fecha: '',
+        sol_estado: ''
     };
 
-    $scope.agregar = function() {
-      $scope.nuevaSolicitud.sol_fecha = new Date();
-      $scope.nuevaSolicitud.sol_estado = 'S';
-      console.log('nuevaSolicitud', $scope.nuevaSolicitud);
+    class Ciudad {
+        constructor(cod, nombre) {
+            this.codigo = cod;
+            this.nombre = nombre;
+        }
     }
-    // $scope.agregarNuevoUsuario = function () {
-    //     UsuarioFactory.save({
-    //         nombre: $scope.nuevoUsuario.nombre,
-    //         apellido: $scope.nuevoUsuario.apellido,
-    //         fechaDeNacimiento: $scope.nuevoUsuario.fechaDeNacimiento,
-    //         email: $scope.nuevoUsuario.email,
-    //         password: $scope.nuevoUsuario.password
-    //     }).$promise.then(
-    //         function success(respuesta) {
-    //             toastr.success('Éxito!', 'Se ingresó el Nuevo Usuario');
-    //             $cookies.put('UsuarioId', respuesta.id);
-    //             $state.go('miperfil');
-    //         },
-    //         function error(error) {
-    //             toastr.error('Error!', 'No se ingresó el Nuevo Usuario');
-    //             console.log(error);
-    //         });
-    // }
+
+    class Provincia {
+        constructor(cod, nombre, ciudades) {
+            this.codigo = cod;
+            this.nombre = nombre;
+        }
+    }
+
+    function generarProvincias() {
+        var ciudades = [];
+        var cod;
+        var nombre;
+        for (var i = 0; i < 15; i++) {
+            cod = i + 1;
+            nombre = 'Ciudad de Generada ' + cod;
+            ciudades.push(new Ciudad(cod, nombre))
+        }
+        return ciudades;
+    }
+
+    var esmeraldas = new Provincia(8, 'Esmeraldas');
+    var manabi = new Provincia(10, 'Manabí');
+    var losrios = new Provincia(12, 'Los Ríos');
+    $scope.provincias = [];
+    $scope.provincias.push(esmeraldas);
+    $scope.provincias.push(manabi);
+    $scope.provincias.push(losrios);
+    console.log($scope.provincias);
+
+    $scope.ciudades = generarProvincias();
+    console.log($scope.ciudades);
+
+    $scope.auxsolicitud = {};
+
+    $scope.agregar = function() {
+        $scope.nuevaSolicitud.sol_fecha = new Date();
+        $scope.nuevaSolicitud.sol_estado = 'S';
+
+        if ($scope.auxsolicitud.sol_disc === 'Si') {
+            $scope.nuevaSolicitud.sol_disc = true;
+        } else {
+            $scope.nuevaSolicitud.sol_disc = false;
+        }
 
 
+        console.log('nuevaSolicitud', $scope.nuevaSolicitud);
+        SolicitudFactory.save({
+          sol_ci: $scope.nuevaSolicitud.sol_ci,
+          sol_apellidos: $scope.nuevaSolicitud.sol_apellidos,
+          sol_nombres: $scope.nuevaSolicitud.sol_nombres,
+          sol_codprov: $scope.nuevaSolicitud.sol_codprov,
+          sol_codciu: $scope.nuevaSolicitud.sol_codciu,
+          sol_sexo: $scope.nuevaSolicitud.sol_sexo,
+          sol_fechanac: $scope.nuevaSolicitud.sol_fechanac,
+          sol_nummiem: $scope.nuevaSolicitud.sol_nummiem,
+          sol_disc: $scope.nuevaSolicitud.sol_disc,
+          sol_telefono: $scope.nuevaSolicitud.sol_telefono,
+          sol_fecha: $scope.nuevaSolicitud.sol_fecha,
+          sol_estado: $scope.nuevaSolicitud.sol_estado
+        }).$promise.then(
+            function success(respuesta) {
+                toastr.success('Éxito!', 'Se ingresó la solicitud');
+                console.log('exito en ingreso de solicitud');
+                $scope.nuevaSolicitud = {};
+            },
+            function error(error) {
+                toastr.error('Error!', 'No se ingresó el Nuevo Usuario');
+                console.log('Error en ingreso de solicitud', error);
+            });
+    }
 }]);
